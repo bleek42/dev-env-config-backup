8e5b21af83d38169cd1254152d1a1440c5aeb5a7
bleek42 <brandonleek42@gmail.com>
2023-09-03 17:29:04 -0400

    updated/formatted/renamed KDE-linux, vsc, wt, alacritty, etc.

--- lib/KDE-ArchLinux/profile.sh
+++ lib/KDE-ArchLinux/profile.sh
@@ -1,8 +1,10 @@
-export LANG=en_US.UTF-8
-export BROWSER="/usr/bin/firefox-developer-edition:$BROWSER"
-export EDITOR="/usr/bin/micro:$EDITOR"
-export VISUAL="/usr/bin/codium"
-export TERM=xterm-256
+#!/usr/bin/env sh
+
+export LANG="en_US.UTF-8"
+export BROWSER=/usr/bin/firefox-developer-edition:"$BROWSER"
+export EDITOR="$VISUAL":/usr/bin/micro
+export VISUAL=/usr/bin/codium
+export TERM="xterm-256"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_DIRS="/usr/share:/usr/local/share:$XDG_DATA_HOME"
 export XDG_CONFIG_HOME="$HOME/.config"
@@ -12,5 +14,4 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export LIBGL_ALWAYS_INDIRECT=1
 export MANPATH="/usr/local/man:$MANPATH"
 export LESS='-F -i -J -M -R -W -x4 -X -z-4'
-export EDITOR='/usr/bin/micro'
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
