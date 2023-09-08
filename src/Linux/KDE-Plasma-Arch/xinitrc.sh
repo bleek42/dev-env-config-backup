c2e895a36327383191b257c12b0e686a4cfb0033
bleek42 <brandonleek42@gmail.com>
2023-09-08 12:41:51 -0400

    more insignificant changes/moving around to make file/folder structure coherent..

--- src/Linux/KDE-Plasma-Arch/xinitrc.sh
+++ src/Linux/KDE-Plasma-Arch/xinitrc.sh
@@ -2,14 +2,14 @@
 #
 # ~/.xinitrc
 #
-# Executed by startx (run your window manager from here)
+# ! Executed by startx (run your window manager from here)
 userresources=$HOME/.Xresources
 usermodmap=$HOME/.Xmodmap
 sysresources=/etc/X11/xinit/.Xresources
 sysmodmap=/etc/X11/xinit/.Xmodmap
 
-if [ -d /etc/X11/xinit/xinitrc.d ] ; then
-    for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+if [ -d /etc/X11/xinit/xinitrc.d ]; then
+    for f in /etc/X11/xinit/xinitrc.d/?*.sh; do
         [ -x "$f" ] && . "$f"
     done
     unset f
@@ -18,12 +18,12 @@ fi
 export DESKTOP_SESSION=plasma
 exec startplasma-x11
 
-# see https://unix.stackexchange.com/a/295652/332452
+# ? see https://unix.stackexchange.com/a/295652/332452
 source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh
 
-# see https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
+# ? see https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
 eval $(/usr/bin/gnome-keyring-daemon --start)
 export SSH_AUTH_SOCK
 
-# see https://github.com/NixOS/nixpkgs/issues/14966#issuecomment-520083836
+# ? see https://github.com/NixOS/nixpkgs/issues/14966#issuecomment-520083836
 mkdir -p "$HOME/.local/share/keyrings"
