b884d94fe606a54c918349178f5d375d25db85f3
bleek42 <brandonleek42@gmail.com>
2023-09-09 23:09:59 -0400

    quick commit zsh profle etc.

--- src/Linux/profile.sh
+++ src/Linux/profile.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env sh
 
-export LANG="en_US.UTF-8"
 # ~/.profile: executed by the command interpreter for login shells.
 # This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
 # exists.
 
 # see /usr/share/doc/bash/examples/startup-files for examples.
 # the files are located in the bash-doc package.
-# the default umask is set in /etc/profile; for setting the umask
 # for ssh logins, install and configure the libpam-umask package.
 
-# umask 022
+# the default umask is set in /etc/profile; for setting the umask
+umask 022
 
+export LANG="en_US.UTF-8"
 export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P'
 # Make less more friendly
 export LESS_ADVANCED_PREPROCESSOR=1
 export LESS='-R -F -i -J -M -W -x4 -z4'
 export PAGER='less'
-export MANPAGER="/bin/sh -c 'col -bx | batcat --language=help -n man -p'"
+export MANPAGER="/sh -c 'col -bx | bat --language=help -n man -p'"
+export MANROFFOPT=-c
 # export MANPAGER=$'/bin/sh -c \'col -b | batcat -p -l man\''
 export EDITOR='/usr/bin/nvim'
 export VISUAL='code'
@@ -25,9 +26,9 @@ export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 perm
 export LESSOPEN="| lesspipe |~/.lessfilter %s 2>&-"
 # export LESSOPEN="|~/.lessfilter %s 2>&-"
 export BROWSER='/mnt/c/Program Files/Mozilla/firefox.exe'
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 # export NODE_PRESERVE_SYMLINKS=1
-export MANROFFOPT=-c
 
 LESS_TERMCAP_md="$(tput setaf 136)"
 [ -e "$LESS_TERMCAP_md" ] && export LESS_TERMCAP_md
@@ -38,8 +39,7 @@ if [ -d "$HOME/.local/bin" ]; then
 fi
 
 export LIBGL_ALWAYS_INDIRECT=1 #GWSL
-export GTK_THEME='Kali-Purple-Dark'
-export GDK_SCALE=1 #GWSL
+export GDK_SCALE=1             #GWSL
 export QT_SCALE_FACTOR=1
 
 DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0
@@ -49,8 +49,6 @@ PULSE_SERVER=tcp:$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit
 
 WSL_IPV4="$(hostname -I | awk '{print $1}' | awk '{printf $0}')"
 [ -e "$WSL_IPV4" ] && export WSL_IPV4
-export LESS='-F -i -J -M -R -W -x4 -X -z-4'
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 # export XDG_DATA_HOME="$HOME/.local/share"
 # export XDG_DATA_DIRS="/usr/share:/usr/local/share:$XDG_DATA_HOME"
 # export XDG_CONFIG_HOME="$HOME/.config"
---
+++ src/Linux/rc.d/common/aliases.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip --color=auto'
+alias wget='wget --no-check-certificate'
+alias dmesgf='dmesg --ctime --follow'
+alias rsynccp="rsync -avz --progress -h"
+alias rsyncmv="rsync -avz --progress -h --remove-source-files"
+alias rsyncupd="rsync -avzu --progress -h"
+alias rsyncnize="rsync -avzu --delete --progress -h"
+
+alias pack='tar --create --gzip --verbose --file'
+alias unpack='tar --extract --gunzip --verbose --file'
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+function set-treex() {
+	if command -v exa >/dev/null 2>&1; then
+		exa --all --long --group --git --links --time-style=long-iso --header --group-directories-first --color-scale --icons --tree --ignore-glob ".git" --color=always "$@" | batcat --plain -n || cat
+	else
+		tree -aC -I '.git' --dirsfirst "$@" | batcat --plain || cat
+	fi
+}
+
+alias treex='set-treex'
+
+if command -v zoxide >/dev/null 2>&1; then
+	eval "$(zoxide init zsh --hook prompt --cmd zd)"
+fi
+
+if command -v fdfind >/dev/null 2>&1; then
+	alias fd='fdfind --hidden --no-ignore --exclude ".git" --follow --color always'
+	alias fdh='fdfind --hidden --follow --color always'
+fi
+
+if command -v batcat >/dev/null 2>&1; then
+	alias bat='batcat --plain --color=always'
+fi
+
+if command -v ag >/dev/null 2>&1; then
+	alias ag='ag --hidden --ignore ".git" -i -g'
+	alias ags='ag --hidden --smart-case -g'
+fi
+
+if command -v rg >/dev/null 2>&1; then
+	alias rg='rg -i --pretty --hidden --no-ignore-vcs'
+fi
+
+if command -v exa >/dev/null 2>&1; then
+	alias ls='exa -a --color-scale --icons'
+	alias la='exa -al --color-scale --icons'
+	alias lt='exa -lT --color-scale --icons'
+	alias ld='exa -al --links --color-scale --icons'
+	alias ll='exa -al --links --color-scale --icons'
+else
+	alias ls='ls --color=auto'
+	alias ll='ls -l'
+	alias la='ls -A'
+	alias l='ls -CF'
+fi
