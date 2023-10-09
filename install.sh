c8e99f0cb7b8149808f7bda5cf3018577a0ef4f9
bleek42 <brandonleek42@gmail.com>
2023-10-09 10:56:11 -0400

    upd windows install script with cut cmd to shorten long uname -s output unknown shell/os detection

--- install.sh
+++ install.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
 ## ? This script is meant to be run from the root of the repo to install & bootstrap all the config/dotfiles for the current user based on the OS/environment.
-op_sys="$(uname -s)"
-echo "Operating System/Shell Environment: ${op_sys}:(${SHELL})"
+os_long="$(uname -s)"
 
-case "${op_sys}" in
+echo "Operating System/Shell Environment: ${os_long}:(${SHELL})"
+
+case "${os_long}" in
 Linux*)
-    op_sys_short="Linux"
-    ;; # ? Linux
+	os_short="Linux"
+	;; # ? Linux
 Darwin*)
-    op_sys_short="Darwin"
-    ;; # ? MacOS
+	os_short="Darwin"
+	;; # ? MacOS
 MSYS_NT*)
-    op_sys_short="MSYS_NT"
-    ;; # ? Windows 10/11, Powershell (v7)
+	os_short="MSYS_NT"
+	;; # ? Windows 10/11, Powershell (v7)
 CYGWIN*)
-    op_sys_short="CYGWIN"
-    ;; # ? Cygwin, Windows 10/11
+	os_short="CYGWIN"
+	;; # ? Cygwin, Windows 10/11
 MINGW64_NT*)
-    op_sys_short="MINGW64_NT" # ? Windows 10/11, Git-Bash/64-bit (MSYS2/MINGW32)
-    ;;
+	os_short="MINGW64_NT" # ? Windows 10/11, Git-Bash/64-bit (MSYS2/MINGW32)
+	;;
 MINGW32_NT*)
-    op_sys_short="MINGW32_NT"
-    ;;
-*) op_sys_short="UNKNOWN_OP_SYS:${op_sys}" ;;
+	os_short="MINGW32_NT"
+	;;
+*) os_short="unknown_os:$(echo "$os_long" | cut -c1-6)" ;;
 esac
 
-echo "Operating System/Shell Environment (shortened): ${op_sys_short}:(${SHELL})"
+echo "Operating System/Shell Environment (shortened): ${os_short}:(${SHELL})"
