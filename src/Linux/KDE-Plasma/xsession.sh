7d8583e6a830b26462de72702f18345761808310
bleek42 <brandonleek42@gmail.com>
2023-10-29 12:01:38 -0400

    updated scripts/wsl-xdg.sh with proper xdg_dirs set; update kde/xsession.sh with kde vars

--- src/Linux/KDE-Plasma/xsession.sh
+++ src/Linux/KDE-Plasma/xsession.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env sh
 #
-# ~/.xinitrc
+# ~/.xsessiun
 #
-# ! Executed by startx (run your window manager from here)
-userresources=$HOME/.Xresources
-usermodmap=$HOME/.Xmodmap
-sysresources=/etc/X11/xinit/.Xresources
-sysmodmap=/etc/X11/xinit/.Xmodmap
+# ! Executed by x-session-manager (run your session manager from here)
+export DESKTOP_SESSION="plasma"
+export LIBGL_ALWAYS_INDIRECT=1
+export GDK_SCALE=1
+export QT_SCALE_FACTOR=1
+export KDEHOME="$XDG_CONFIG_HOME"/kde
+test -n "$WAYLAND_DISPLAY" && export WAYLAND_DISPLAY=""
 
-export DESKTOP_SESSION=plasma
-exec startplasma-x11
+userresources="${HOME}/.Xresource"s
+usermodmap="${HOME}/.Xmodmap"
+sysresources=/etc/X11/xinit/Xresources
+sysmodmap=/etc/X11/xinit/Xmodmap
+
+# export GTK_THEME=''
+# export GTK_IM_MODULE='ibus'
 
 # ? see https://unix.stackexchange.com/a/295652/332452
-source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh
+# source /etc/X11/Xsession
 
-# ? see https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
-eval $(/usr/bin/gnome-keyring-daemon --start)
-export SSH_AUTH_SOCK
+# # ? see https://wiki.archlinux.org/title/GNOME/Keyring#xinitrc
+# eval $(/usr/bin/gnome-keyring-daemon --start)
+# export SSH_AUTH_SOCK
 
 # ? see https://github.com/NixOS/nixpkgs/issues/14966#issuecomment-520083836
-mkdir -p "$HOME/.local/share/keyrings"
+# test -d "$HOME/.local/share/keyrings" || mkdir -p "$HOME/.local/share/keyrings"
+
+exec x-session-manager &
