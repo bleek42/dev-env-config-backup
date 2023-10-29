8584bd00dd4c519a4e47ca4efbbaed1defe67774
bleek42 <brandonleek42@gmail.com>
2023-10-29 13:48:58 -0400

    rm extra profile.sh

--- src/Linux/common/profile.sh
+++ src/Linux/common/profile.sh
@@ -1,57 +1,30 @@
-#!/usr/bin/env sh
-
 # ~/.profile: executed by the command interpreter for login shells.
 # This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
 # exists.
-
 # see /usr/share/doc/bash/examples/startup-files for examples.
 # the files are located in the bash-doc package.
-# for ssh logins, install and configure the libpam-umask package.
-
 # the default umask is set in /etc/profile; for setting the umask
-umask 022
+# for ssh logins, install and configure the libpam-umask package.
+# umask 022
+is_system_running="$(systemctl status ssh.service)"
+echo "$is_system_running"
 
-export LANG="en_US.UTF-8"
-export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P'
-# Make less more friendly
-export LESS_ADVANCED_PREPROCESSOR=1
-export LESS='-R -F -i -J -M -W -x4 -z4'
-export PAGER='less'
-export MANPAGER="/sh -c 'col -bx | bat --language=help -n man -p'"
-export MANROFFOPT=-c
-# export MANPAGER=$'/bin/sh -c \'col -b | batcat -p -l man\''
-export EDITOR='/usr/bin/nvim'
-export VISUAL='code'
-export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 permission
-export LESSOPEN="| lesspipe |~/.lessfilter %s 2>&-"
-# export LESSOPEN="|~/.lessfilter %s 2>&-"
-export BROWSER='/mnt/c/Program Files/Mozilla/firefox.exe'
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+if [[ -n "$(command -v keychain)" ]] && [[ "$is_system_running" =~ "running" ]]; then
+    eval "$(keychain --systemd --confhost --inherit any --gpg2 --timeout 300 --dir "${HOME}"/.config/keychain --absolute --env "${HOME}"/.config/keychain/"${HOST}"-sh)"
+else
+    eval "$(keychain --nogui --confhost --inherit any --gpg2 --timeout 300 --dir "${HOME}"/.config/keychain --absolute --env "${HOME}"/.config/keychain/"${HOST}"-sh)"
+fi
 
-# export NODE_PRESERVE_SYMLINKS=1
+PULSE_SERVER=tcp:"$(tr </etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')":1
+export PULSE_SERVER
 
-LESS_TERMCAP_md="$(tput setaf 136)"
-[ -e "$LESS_TERMCAP_md" ] && export LESS_TERMCAP_md
+WSL_IPV4="$(hostname -I | awk '{print $1}' | awk '{printf $0}')"
+export WSL_IPV4
 
+export DISPLAY="${HOST}":0
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/.local/bin" ]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
 
 export LIBGL_ALWAYS_INDIRECT=1 #GWSL
-export GDK_SCALE=1             #GWSL
-export QT_SCALE_FACTOR=1
-
-DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0
-PULSE_SERVER=tcp:$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):1
-[ -e "$DISPLAY" ] && export DISPLAY
-[ -e "$PULSE_SERVER" ] && export PULSE_SERVER
-
-WSL_IPV4="$(hostname -I | awk '{print $1}' | awk '{printf $0}')"
-[ -e "$WSL_IPV4" ] && export WSL_IPV4
-# export XDG_DATA_HOME="$HOME/.local/share"
-# export XDG_DATA_DIRS="/usr/share:/usr/local/share:$XDG_DATA_HOME"
-# export XDG_CONFIG_HOME="$HOME/.config"
-# export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_HOME"
-# export XDG_STATE_HOME="$HOME/.local/state"
-# export XDG_CACHE_HOME="$HOME/.cache"
