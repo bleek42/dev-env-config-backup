506d79bba675d5d20faee2f8c36caf9ae4cd9e10
bleek42 <brandonleek42@gmail.com>
2023-11-09 15:19:03 -0500

    updated zsh dotfiles, profile.d/wsl-xdg.sh; set hard symlinks to all kali zsh dotfiles, fzf-config

--- src/Linux/Debian/Kali/zsh/zshenv.zsh
+++ src/Linux/Debian/Kali/zsh/zshenv.zsh
@@ -1,29 +1,23 @@
 #!/usr/bin/env zsh
 
+# configure `time` format
+export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 permissions
+export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P'
+export PAGER='/usr/bin/less'
+export LESS='-R -F -i -J -M -W -x4 -z4'
+export LESSOPEN="|~/.lessfilter %s 2>&-"
+export EDITOR='vim'
+export VISUAL='code'
+export MANROFFOPT=-c
+
+# # ZPLUG
 export ZPLUG_ROOT=/usr/share/zplug
-export ZPLUG_HOME="${HOME}/.config/zplug"
-export ZPLUG_BIN="${HOME}/.config/zplug/bin"
-export ZPLUG_REPOS="${HOME}/.config/zplug/repos"
-export ZPLUG_ERROR_LOG="${HOME}/.config/zplug/error_zplug.log"
-export ZPLUG_CACHE_DIR="${HOME}/.config/zplug/cache"
-export ZPLUG_LOADFILE="${HOME}/.config/zplug/packages.zsh"
+export ZPLUG_HOME="$HOME/.config/zplug"
+export ZPLUG_BIN="$ZPLUG_HOME/bin"
+export ZPLUG_LOADFILE="$ZPLUG_HOME/packages.zsh"
+export ZPLUG_CACHE_DIR="$ZPLUG_HOME/cache"
+export ZPLUG_USE_CACHE=true
+export ZPLUG_ERROR_LOG="$ZPLUG_HOME/error_zplug.log"
+export ZPLUG_REPOS="$ZPLUG_HOME/repos"
 export ZPLUG_PROTOCOL=HTTPS
 export ZPLUG_FILTER=fzf:fzf-tmux
-export ZPLUG_USE_CACHE=true
-export ZPLUG_THREADS=16
-
-# export VIRTUAL_ENV_DISABLE_PROMPT=1
-
-export LESS='-R -F -i -J -M -W -x4 -z4'
-export PAGER='/usr/bin/less'
-export LESSOPEN="|~/.lessfilter %s 2>&-"
-# export LESSOPEN="|~/.lessfilter %s 2>&-"
-
-export LESS_ADVANCED_PREPROCESSOR=1
-export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P'
-export MANPAGER=$'/bin/sh -c \'col -b | batcat -p -l man\''
-# export MANPAGER="/bin/sh -c 'col -bx |bat --language=help -n man -p'"
-export MANROFFOPT=-c
-export EDITOR='/usr/bin/vim'
-export VISUAL='code.exe'
-export BROWSER='/mnt/c/Program Files/Firefox Developer Edition/firefox.exe'
