28a6a71f9a5a4bbc0d0a9aa578ddcd2fb9a21c40
bleek42 <brandonleek42@gmail.com>
2023-11-13 14:43:16 -0500

    add kali/zsh zshenv.sh

--- src/Linux/Debian/Kali/zsh/zshenv.zsh
+++ src/Linux/Debian/Kali/zsh/zshenv.zsh
@@ -13,11 +13,11 @@ export MANROFFOPT=-c
 # # ZPLUG
 export ZPLUG_ROOT=/usr/share/zplug
 export ZPLUG_HOME="$HOME/.config/zplug"
-export ZPLUG_BIN="$ZPLUG_HOME/bin"
-export ZPLUG_LOADFILE="$ZPLUG_HOME/packages.zsh"
-export ZPLUG_CACHE_DIR="$ZPLUG_HOME/cache"
+export ZPLUG_BIN="${ZPLUG_HOME}/bin"
+export ZPLUG_LOADFILE="${ZPLUG_HOME}/packages.zsh"
+export ZPLUG_CACHE_DIR="${ZPLUG_HOME}/cache"
 export ZPLUG_USE_CACHE=true
-export ZPLUG_ERROR_LOG="$ZPLUG_HOME/error_zplug.log"
-export ZPLUG_REPOS="$ZPLUG_HOME/repos"
+export ZPLUG_ERROR_LOG="${ZPLUG_HOME}/error_zplug.log"
+export ZPLUG_REPOS="${ZPLUG_HOME}/repos"
 export ZPLUG_PROTOCOL=HTTPS
 export ZPLUG_FILTER=fzf:fzf-tmux
